78c6028743785982660ea14a5598841b19753365
SKSARUK2000 <159781378+SKSARUK2000@users.noreply.github.com>
2024-02-12 19:47:08 +0530

    Updated

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoute from "./routes/authRoute.js";
 import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import path from 'path'
 import {fileURLToPath} from "url";
 
 //configure env---
