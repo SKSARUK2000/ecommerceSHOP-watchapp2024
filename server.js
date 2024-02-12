fe094a8f36a5bd4e76d99a67d7965a1d343c37e0
SKSARUK2000 <159781378+SKSARUK2000@users.noreply.github.com>
2024-02-12 19:35:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,15 @@ import authRoute from "./routes/authRoute.js";
 import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-import path from "path";
+import {fileURLToPath} from "url";
 
 //configure env---
 dotenv.config();
 
 //connect Database
 connectDB();
-
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
 const app = express();
 
 //middlewares--
